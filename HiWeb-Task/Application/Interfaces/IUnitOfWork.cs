d8dd70a5eab10a2947ab1e442f64416ba34c75c2
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2024-03-18 00:14:05 +0330

    Add repositories to UnitOfWork pattern

--- HiWeb-Task/Application/Interfaces/IUnitOfWork.cs
+++ HiWeb-Task/Application/Interfaces/IUnitOfWork.cs
@@ -1,5 +1,15 @@
+using HiWeb_Task.Application.Interfaces.Repositories.Course;
+using HiWeb_Task.Application.Interfaces.Repositories.Professor;
+using HiWeb_Task.Application.Interfaces.Repositories.Student;
+using HiWeb_Task.Application.Interfaces.Repositories.Term;
+
 namespace HiWeb_Task.Application.Interfaces;
 
 public interface IUnitOfWork : IDisposable
 {
+    public IStudentRepository Students { get; set; }
+    public IProfessorRepository Professors { get; set; }
+    public ITermRepository Terms { get; set; }
+    public ICourseRepository Courses { get; set; }
+    Task<bool> CommitAsync();
 }
--- HiWeb-Task/Infrastructure/UnitOfWork.cs
+++ HiWeb-Task/Infrastructure/UnitOfWork.cs
@@ -1,15 +1,32 @@
 using HiWeb_Task.Application.Interfaces;
+using HiWeb_Task.Application.Interfaces.Repositories.Course;
+using HiWeb_Task.Application.Interfaces.Repositories.Professor;
+using HiWeb_Task.Application.Interfaces.Repositories.Student;
+using HiWeb_Task.Application.Interfaces.Repositories.Term;
+using HiWeb_Task.Infrastructure.Repository.Course;
+using HiWeb_Task.Infrastructure.Repository.Professors;
+using HiWeb_Task.Infrastructure.Repository.Students;
+using HiWeb_Task.Infrastructure.Repository.Term;
 
 namespace HiWeb_Task.Infrastructure;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _context;
-    
-    
+
+    public IStudentRepository Students { get; set; }
+    public IProfessorRepository Professors { get; set; }
+    public ITermRepository Terms { get; set; }
+    public ICourseRepository Courses { get; set; }
+
     public UnitOfWork(AppDbContext context)
     {
         _context = context;
+
+        Students = new StudentRepository(_context);
+        Professors = new ProfessorRepository(_context);
+        Terms = new TermRepository(_context);
+        Courses = new CourseRepository(_context);
     }
     
     public async Task<bool> CommitAsync()
