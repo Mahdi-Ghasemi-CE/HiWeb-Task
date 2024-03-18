a8e9629a4c1cfdcbf5e1ecc56a8348d34bfd00f2
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2024-03-18 04:00:19 +0330

    Add - Get by id in student repository

--- HiWeb-Task/Application/Interfaces/Repositories/Students/IStudentRepository.cs
+++ HiWeb-Task/Application/Interfaces/Repositories/Students/IStudentRepository.cs
@@ -3,4 +3,5 @@ namespace HiWeb_Task.Application.Interfaces.Repositories.Student;
 public interface IStudentRepository : IRepository<Domain.Student.Student>
 {
     Task<Domain.Student.Student> StudentsWithJointCourseInTerm();
+    Task<Domain.Student.Student> Get(int id);
 }
--- HiWeb-Task/Infrastructure/Repository/Students/StudentRepository.cs
+++ HiWeb-Task/Infrastructure/Repository/Students/StudentRepository.cs
@@ -1,5 +1,6 @@
 using HiWeb_Task.Application.Interfaces.Repositories.Student;
 using HiWeb_Task.Domain.Student;
+using Microsoft.EntityFrameworkCore;
 
 namespace HiWeb_Task.Infrastructure.Repository.Students;
 
@@ -15,5 +16,10 @@ public class StudentRepository :Repository<Student>,IStudentRepository
     public Task<Student> StudentsWithJointCourseInTerm()
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<Student> Get(int id)
+    {
+        return await _queryable.SingleOrDefaultAsync(s => s.StudentId == id);
     }
 }
