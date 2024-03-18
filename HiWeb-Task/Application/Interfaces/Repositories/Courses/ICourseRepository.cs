fb5d5beb248f3399cb71b0cf9d4c124cac6d69c3
Mahdi-Ghasemi-CE <mahdighasemi.shahed@gmail.com>
2024-03-18 05:33:48 +0330

    Add - Get by id in course repository

--- HiWeb-Task/Application/Interfaces/Repositories/Courses/ICourseRepository.cs
+++ HiWeb-Task/Application/Interfaces/Repositories/Courses/ICourseRepository.cs
@@ -2,4 +2,5 @@ namespace HiWeb_Task.Application.Interfaces.Repositories.Course;
 
 public interface ICourseRepository : IRepository<Domain.Course.Course>
 {
+    Task<Domain.Course.Course> Get(int id);
 }
--- HiWeb-Task/Infrastructure/Repository/Courses/CourseRepository.cs
+++ HiWeb-Task/Infrastructure/Repository/Courses/CourseRepository.cs
@@ -1,4 +1,5 @@
 using HiWeb_Task.Application.Interfaces.Repositories.Course;
+using Microsoft.EntityFrameworkCore;
 
 namespace HiWeb_Task.Infrastructure.Repository.Course;
 
@@ -9,5 +10,18 @@ public class CourseRepository : Repository<Domain.Course.Course>,ICourseReposito
     public CourseRepository(AppDbContext dbContext) : base(dbContext)
     {
         _queryable = dbContext.Set<Domain.Course.Course>();
+    }
+
+    public async Task<Domain.Course.Course> Get(int id)
+    {
+        var course = 
+            await 
+                _dbContext
+                .Courses
+                .Include(C => C.Professor)
+                .Include(C => C.Term)
+                .Include(C => C.Students)
+                .FirstOrDefaultAsync();
+        return course;
     }
 }
